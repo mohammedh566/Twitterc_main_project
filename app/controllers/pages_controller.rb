65e0db288106d7dc1e41a44777af57ee4e512ab0
Jithin Das <jithindasad@gmail.com>
2017-10-24 22:50:48 +0530

    Finish GUI

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,31 +1,35 @@
 class PagesController < ApplicationController
   # back-end code for pages/index
-    def index
-    end
+   def index
+   end
 
-    # back-end code for pages/home
-    def home
-      @posts = Post.all
-      @newPost = Post.new
-    end
+   # back-end code for pages/home
+   def home
+     @posts = Post.all
+     @newPost = Post.new
+   end
 
-    # back-end code for pages/profile
-    def profile
-      # grab the username from the URL as :id
-      if (User.find_by_username(params[:id]))
-        @username = params[:id]
-      else
-        # redirect to 404 (root for now)
-        redirect_to root_path, :notice=> "User not found!"
-      end
+   # back-end code for pages/profile
+   def profile
+     # grab the username from the URL as :id
+     if (User.find_by_username(params[:id]))
+       @username = params[:id]
+     else
+       # redirect to 404 (root for now)
+       redirect_to root_path, :notice=> "User not found!"
+     end
 
-      @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
-      @newPost = Post.new
-    end
+     @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
+     @newPost = Post.new
 
-    # back-end code for pages/explore
-    def exxplore
-      @posts = Post.all
-    end
+     @toFollow = User.all.last(5)
+   end
 
-  end
+   # back-end code for pages/explore
+   def exxplore
+     @posts = Post.all
+     @newPost = Post.new
+     @toFollow = User.all.last(5)
+   end
+
+ end
