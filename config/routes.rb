8e48aba6956672dcd2424834da87840f56985559
Jithin Das <jithindasad@gmail.com>
2017-10-24 23:28:39 +0530

    followers added

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_for :models
-  resource :posts
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :relationships, only: [:create, :destroy]
+  resources :posts
+  # Define Root URL
   root 'pages#index'
 
-  get '/home' => 'pages#home'
-
+  # Define Routes for Pages
+  get '/home' => 'pages#home' # override default routes.
   get '/user/:id' => 'pages#profile'
   get '/exxplore' => 'pages#exxplore'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171023172623) do
+ActiveRecord::Schema.define(version: 20171024173953) do
 
   create_table "models", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -36,6 +36,16 @@ ActiveRecord::Schema.define(version: 20171023172623) do
     t.datetime "updated_at", null: false
     t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
